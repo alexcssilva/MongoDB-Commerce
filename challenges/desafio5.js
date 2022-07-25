c89e6c2497f12e78788623316c72f467e2d7df9a
Alex Silva <alexcssilva@gmail.com>
2022-07-25 17:49:44 -0300

    feat: added Desafio 05

---
+++ challenges/desafio5.js
@@ -0,0 +1,14 @@
+// 5 - Retorne o nome, as curtidas e vendidos dos lanches que tiveram quantidade de curtidas igual a 36 ou tenham a quantidade de vendas igual a 85.
+
+db.produtos.find({
+  $and: [
+    { $or: [{
+        curtidas: { $eq: 36 } }, {
+        vendidos: { $eq: 85 } }] },
+  ],
+}, {
+  _id: 0,
+  nome: 1,
+  curtidas: 1,
+  vendidos: 1,
+});
