dc40f36325b278bcac867960a9b529ccde2c559c
Alex Silva <alexcssilva@gmail.com>
2022-07-26 18:49:14 -0300

    feat: added Desafio 17

---
+++ challenges/desafio17.js
@@ -0,0 +1,13 @@
+// 17 - Retorne a quantidade total de produtos em uma nova coleção chamada resumoProdutos
+
+db.resumoProdutos.insertOne({
+  franquia: "McDonalds",
+  totalProdutos: 5,
+});
+
+db.resumoProdutos.find({},
+  {
+    _id: 0,
+    franquia: 1,
+    totalProdutos: 1,
+});
