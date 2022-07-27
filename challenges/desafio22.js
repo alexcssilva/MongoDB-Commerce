03d17d9d1c24b2750198de70d133d15775d0b7b6
Alex Silva <alexcssilva@gmail.com>
2022-07-26 22:59:32 -0300

    feat: added Desafio 22

---
+++ challenges/desafio22.js
@@ -0,0 +1,33 @@
+// 22 - Adicione a quantidade de vendas dos sanduíches por dia da semana
+
+db.produtos.find();
+
+db.produtos.updateMany({},
+  {
+    $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] },
+  });
+
+db.produtos.updateOne({
+  nome: { $eq: "Big Mac" },
+},
+  {
+    $set: {
+      "vendasPorDia.3": 60,
+    },
+});
+
+db.produtos.updateMany({
+  tags: { $eq: "bovino" },
+},
+  {
+    $set: {
+      "vendasPorDia.6": 120,
+    },
+});
+
+db.produtos.find({},
+{
+  _id: 0,
+  nome: 1,
+  vendasPorDia: 1,
+});
